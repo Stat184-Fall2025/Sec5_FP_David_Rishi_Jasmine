879b65d36de20aec096b413aad56bc115ac39050
dpg5549 <dpg5549@psu.edu>
2025-12-04 11:12:11 -0500

    test

--- Project_Init.R
+++ Project_Init.R
@@ -1,11 +1,17 @@
 install.packages("tidyquant")
 library(tidyquant)
-tickers <- c("AAPL", "TSM", "QCOM", "SWKS", "AVGO", "TXN")
+tickers <- c("AAPL")
+tickers1 <- c("HNHPF")
 start_date <- "2022-01-01"
 end_date   <- Sys.Date() # Today
 
 
-stock_data <- tq_get(tickers, 
+appl_data <- tq_get(tickers, 
                      get  = "stock.prices", 
                      from = start_date, 
                      to   = end_date)
+
+foxconn_data <- tq_get(tickers1, 
+                    get  = "stock.prices", 
+                    from = start_date, 
+                    to   = end_date)
